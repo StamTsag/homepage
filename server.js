66eff4f019f27d1a8628079c208013d5c7960e45
Shadofer <shadowrlrs@gmail.com>
2021-08-23 17:11:37 +0300

    Add semi-mobile support.

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ server.get("*", function(req, res) {
   
     res.write(`
 <!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel='icon' type='image/png' href='favicon.png'/>
 <link rel='stylesheet' href='/global.css'>
 <link rel='stylesheet' href='generated/bundle.css'>
+
 <div id="app">${html}
 </div>
 <script src="generated/bundle.js"></script>

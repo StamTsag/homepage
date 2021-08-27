b58d5f157518159b54971b850f0381f005262c1b
Shadofer <shadowrlrs@gmail.com>
2021-08-27 16:15:50 +0300

    Add project categories to properly seperate.

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -11,18 +11,8 @@
 
 <h1 class='top-text'>My projects</h1>
 
-<h1>Most recent</h1>
-
-<div class='top-project'>
-	<div class='project-top'>
-		<img class='project-icon' src='../../images/towerverse-icon.png' alt='TowerVerse project icon'/>
-		<h1 class='project-title'><a href='https://github.com/TowerVerse'>TowerVerse</a></h1>
-	</div>
-
-	<h3 class='project-description'>An online, cross-platform, medieval-themed game, where players go against each other in various tag game modes!</h3>
-</div>
-
-<h1 style='margin-top: 50px;'>Older projects</h1>
+<!-- Software development -->
+<h1 class='project-category' style='color: #43d897;'>Software development</h1>
 
 <div class='top-project'>
 	<h1 class='project-title'><a href='https://github.com/Shadofer/dogey'>Dogey</a></h1>
@@ -35,4 +25,28 @@
 <div class='project'>
 	<h1 class='project-title'><a href='https://github.com/Shadofer/basicpyapi'>Basicpyapi</a></h1>
 	<h3 class='project-description'>A barebones websocket server & client made in python.</h3>
+</div>
+
+<!-- Web development-->
+<h1 class='project-category' style='color: #107ee5;'>Web development</h1>
+
+<div class='top-project'>
+	<div class='project-top'>
+		<img class='project-icon' src='../../favicon.png' alt='Shadofer homepage project icon'/>
+		<h1 class='project-title'><a href='https://github.com/Shadofer/homepage'>Shadofer homepage</a></h1>
+	</div>
+
+	<h3 class='project-description'>The source code of this site, made with <a href='https://svelte.dev'>Svelte</a>.</h3>
+</div>
+
+<!-- Game development -->
+<h1 class='project-category' style='color: #e53a10'>Game development</h1>
+
+<div class='top-project'>
+	<div class='project-top'>
+		<img class='project-icon' src='../../images/towerverse-icon.png' alt='TowerVerse project icon'/>
+		<h1 class='project-title'><a href='https://github.com/TowerVerse'>TowerVerse</a></h1>
+	</div>
+
+	<h3 class='project-description'>An online, cross-platform, medieval-themed game, where players go against each other in various tag game modes!</h3>
 </div>

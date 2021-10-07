b6e92d738894b70e399ce1c12d7db7e12e1cd2aa
shadofer <shadowrlrs@gmail.com>
2021-10-07 23:16:13 +0300

    Add error page, warn if in dev mode.

---
+++ src/routes/__error.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+                status: `${status}`,
+                err: `${error.message}`
+			}
+		};
+	}
+</script>
+
+<script>
+    export let err, status;
+</script>
+
+<h1 style='text-align: center; font-size: 6vh;'>{status}</h1>
+<h1 style='text-align: center; font-size: 5vh;'>{err}</h1>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,15 @@
 <script>
+    import { dev } from '$app/env';
     import { goto } from '$app/navigation';
 
     import '../app.css';
 </script>
 
 <h1 class='top logo' on:click={() => goto('/')}>Shadofer</h1>
+
+<!-- warn-->
+{#if dev}
+    <h1 style='padding: 0; margin: 0; text-align: center;'>DEVELOPMENT ENVIRONMENT</h1>
+{/if}
 
 <slot></slot>

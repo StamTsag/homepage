2a489377cfde7f051afffcc21669a7216a08da67
Shadofer <shadowrlrs@gmail.com>
2021-12-27 15:29:20 +0200

    Remove development mode warning.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,9 @@
 <script>
     import '../app.css';
 
-    import { dev } from '$app/env';
     import { goto } from '$app/navigation';
 </script>
 
 <h1 class='top logo' on:click={() => goto('/')}>Shadofer</h1>
-
-{#if dev}
-    <h1 style='padding: 0; margin: 0; text-align: center;'>DEVELOPMENT ENVIRONMENT</h1>
-{/if}
 
 <slot></slot>

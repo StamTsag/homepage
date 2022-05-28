53b8c68df53d200d1703589a92ed5d03da3c735b
Shadofer <shadowrlrs@gmail.com>
2022-05-28 14:55:16 +0300

    Create TopNav component

---
+++ src/lib/TopNav.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import 'styles/top-nav.css';
+    import { fly } from 'svelte/transition';
+</script>
+
+<div
+    in:fly={{ y: -100, duration: 750, opacity: 0.5 }}
+    class="top-nav-container"
+>
+    <div class="info-container">
+        <img
+            src="images/pfp.png"
+            alt="Shadofer logo"
+            width="48px"
+            height="48px"
+        />
+        <h1>Shadofer</h1>
+    </div>
+
+    <div class="categories-container">
+        <h1>About me</h1>
+        <h1>Projects</h1>
+        <h1>Connections</h1>
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
+    import TopNav from 'src/lib/TopNav.svelte';
+    import { onMount } from 'svelte';
+
+    let mountReady = false;
+
+    onMount(() => {
+        mountReady = true;
+    });
 </script>
 
 <svelte:head>
     <title>Shadofer Homepage</title>
 </svelte:head>
+
+{#if mountReady}
+    <TopNav />
+{/if}

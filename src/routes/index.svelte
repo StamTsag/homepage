fae7b940f4c53cf29315d957351fc566c78b2136
Shadofer <shadowrlrs@gmail.com>
2022-05-28 12:53:40 +0300

    Provide a new clean slate for the new homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,6 @@
 <script lang="ts">
-    import Wave from 'src/lib/Wave.svelte';
-    import { onMount } from 'svelte';
-
-    let mountReady = false;
-
-    onMount(() => {
-        mountReady = true;
-    });
 </script>
 
 <svelte:head>
     <title>Shadofer Homepage</title>
 </svelte:head>
-
-{#if mountReady}
-    <Wave waveColor="#ffffff" />
-
-    <div class="center-container">
-        <h1>Hello there!</h1>
-    </div>
-{/if}

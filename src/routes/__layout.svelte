be51fda44b3634592d44dc61a6d2196611db19f9
Shadofer <shadowrlrs@gmail.com>
2022-05-28 18:45:12 +0300

    Add support for themes to existing styles, update scrollbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,4 +12,18 @@
     :global(body) {
         background-color: var(--theme-bg_color);
     }
+
+    :global(h1) {
+        color: var(--theme-text_color);
+    }
+
+    :global(button) {
+        background: var(--theme-button_bg_color);
+        border: 7px solid var(--theme-button_bg_color);
+        box-shadow: 0 0 5px var(--theme-button_bg_color);
+    }
+
+    :global(button):hover {
+        box-shadow: 0 0 15px var(--theme-button_bg_color);
+    }
 </style>
--- src/themes.js
+++ src/themes.js
@@ -1,10 +1,14 @@
 const themes = {
     light: {
         bg_color: '#ffffff',
+        text_color: '#000000',
+        button_bg_color: '#000000',
     },
 
     dark: {
         bg_color: 'rgba(0, 0, 0, 0.918)',
+        text_color: '#ffffff',
+        button_bg_color: '#ffffff',
     },
 };
 

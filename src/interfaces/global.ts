57f30c1ca32d3e99882ccf533613846eaad74390
Shadofer <shadowrlrs@gmail.com>
2022-05-28 12:51:23 +0300

    Remove unused interfaces

--- src/interfaces/global.ts
+++ src/interfaces/global.ts
@@ -2,14 +2,4 @@
 // Shared interfaces for all kinds of files.
 // ******************** //
 
-export interface ErrorLoadParams {
-    status: number;
-    error: Error;
-}
-
-export interface ErrorLoadResult {
-    props: {
-        status: number;
-        error: string;
-    };
-}
+export {};

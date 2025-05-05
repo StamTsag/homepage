47e9823c8addac0aa178ca5911cd0230a1cf089b
StamTsag <shadowrlrs@gmail.com>
2025-05-05 16:05:24 +0300

    Remove project

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -4,6 +4,15 @@ import ProjectCard from "./project-card";
 export default function Projects() {
   const projects: Project[] = [
     {
+      title: "Acrylica",
+      description:
+        "A minimalistic, acrylic-based design operating system on the web.",
+      github: "https://github.com/StamTsag/acrylica",
+      link: "https://acrylica.stamtsag.com",
+      tags: ["SvelteKit", "shadcn", "Tailwind"],
+      usePng: true,
+    },
+    {
       title: "Speedy",
       description:
         "Measure your internet speed efficiently and share your results.",
@@ -14,29 +23,12 @@ export default function Projects() {
       usePng: true,
     },
     {
-      title: "TuneXify",
-      description:
-        "Instantly download music from both Youtube & Spotify with just a link.",
-      link: "https://tunexify.stamtsag.com",
-      tags: ["Next.JS", "shadcn", "Tailwind"],
-      usePng: true,
-    },
-    {
       title: "LinkShr",
       description:
         "Shorten those long links to a simple readable format, online.",
       link: "https://linkshr.stamtsag.com",
       tags: ["Next.JS", "shadcn", "Tailwind", "Java", "Spring Boot"],
       github: "https://github.com/StamTsag/linkshr",
-      usePng: true,
-    },
-    {
-      title: "Acrylica",
-      description:
-        "A minimalistic, acrylic-based design operating system on the web.",
-      github: "https://github.com/StamTsag/acrylica",
-      link: "https://acrylica.stamtsag.com",
-      tags: ["SvelteKit", "shadcn", "Tailwind"],
       usePng: true,
     },
   ];

d19236e56daf32655a6bb702926a7a09030f4d90
StamTsag <shadowrlrs@gmail.com>
2025-06-30 17:28:37 +0300

    Fix map key

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -62,7 +62,7 @@ export default function Projects() {
         </h2>
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
           {projects.map((project) => {
-            return <ProjectCard {...project} />;
+            return <ProjectCard key={project.title} {...project} />;
           })}
         </div>
       </div>
